503dda94c168f538d8311290d081946aeb49ca2d
Dinesh Kumar <gdineshda@gmail.com>
2025-05-25 23:09:24 +0530

    API: fix bug, add futures::FutureUnordered

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod types;
 
 use app::Application;
 use clap::Parser;
+use futures::{stream::FuturesUnordered, StreamExt};
 use miss_me::Index;
 use tokio::fs::DirEntry;
 use types::Args;
@@ -16,22 +17,28 @@ async fn main() -> std::io::Result<()> {
     };
 
     let mut indices = Vec::new();
-
+    let mut tasks = FuturesUnordered::new();
     let mut handle = tokio::fs::read_dir(path).await?;
     while let Some(dir_entry) = handle.next_entry().await? {
-        let output = tokio::spawn(append_index(dir_entry));
-        if let Ok(Some(index)) = output.await {
-            indices.push(index);
+        tasks.push(Box::pin(append_index(dir_entry)));
+        // let output = tokio::spawn(append_index(dir_entry));
+    }
+
+    while let Some(stream_output) = tasks.next().await {
+        if let Some(output) = stream_output {
+            indices.push(output);
         }
     }
 
-    let mut terminal = ratatui::init();
-    let mut app = Application::new(indices);
-
-    let result = app.run(&mut terminal);
-
-    ratatui::restore();
-    result
+    // let mut terminal = ratatui::init();
+    // let mut app = Application::new(indices);
+    //
+    // let result = app.run(&mut terminal);
+    //
+    // ratatui::restore();
+    // result
+    println!("{:#?}", indices);
+    Ok(())
 }
 
 async fn append_index(dir_entry: DirEntry) -> Option<Index> {
